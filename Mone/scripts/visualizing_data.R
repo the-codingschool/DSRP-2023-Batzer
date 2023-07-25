2f27ee20447243c5cc10014f0aa49875e1e37b9f
Mone Abbott <moneabbott@gmail.com>
2023-07-25 16:01:11 -0400

    Update visualizing_data: Created Numeric Data + Total Emissions Data + Population Data & Plots + Correlation Plots & Data Frame

--- Mone/scripts/visualizing_data.R
+++ Mone/scripts/visualizing_data.R
@@ -1,7 +1,9 @@
 # Reading in Data 
 Eu_co2 = read.csv("Data/carbonmonitor-eu_datas_2023-07-19.csv")
+population = read.csv("Data/worldbank_population_bycountry - data.csv")
 
 View(Eu_co2)
+View(population)
 
 ## Load in packages ####
 library(lubridate)
@@ -9,6 +11,10 @@ library(tidyr)
 library(janitor)
 library(dplyr)
 library(ggplot2)
+library(ranger)
+library(reshape2)
+library(yardstick)
+library(Metrics)
 
 ###########################################################################
 # Changining class type ###
@@ -111,3 +117,83 @@ ggplot( data = no_uk_carbon, aes( x = year, y = value, fill = year)) +
     x = "Year"
   ) + 
   theme_minimal()
+
+# Total emissions data ####
+carbon = Eu_co2 |>
+  pivot_wider(
+    names_from = sector, 
+    values_from = value
+  ) |>
+  mutate(
+    total_em = Power + Industry + `Ground Transport`+ Residential +
+                   `Domestic Aviation` + `International Aviation`
+  )
+
+
+### Population Data #### 
+
+country_pop = pivot_wider(
+  population,
+  names_from = country,
+  values_from = population
+)
+
+
+no_uk_pop = population |>
+  filter(country != c("United Kingdom", "EU27 & UK"))
+
+# ploting the population data in bar plot 
+ggplot( data = no_uk_pop, aes( x = year, y = population, fill = year)) + 
+  geom_bar(stat = "sum", na.rm = FALSE) + 
+  labs(
+    title = "Total European Union Population per year (2019 - 2023)",
+    y = "Population",
+    x = "Year"
+  ) + 
+  ylim(7.5e+08, 1.0e+09) # work on getting this to work!!  + 
+  theme_minimal()
+  
+# ploting the population data in density plot 
+  ggplot(no_uk_pop, aes( x = year)) +
+    geom_density() +
+    labs(
+      title = "Total European Union Population per year (2019 - 2023)",
+      y = "Population",
+      x = "Year"
+    ) +  
+    theme_minimal()
+  
+
+## Making numeric data 
+  carbon_all_numeric = pivot_wider(Eu_co2, 
+                                   names_from = sector,
+                                   values_from = value)
+  
+# turning year & integer into a numeric variable 
+carbon_all_numeric = carbon_all_numeric |>
+  mutate(year = as.integer(as.factor(year))) |>
+  mutate(country = as.integer(as.factor(country))) |>
+  # adding total emission data 
+  mutate(
+    total_em = Power + Industry + `Ground Transport`+ Residential +
+      `Domestic Aviation` + `International Aviation`
+  )
+
+# removing data & timestamp from data set 
+carbon_all_numeric = carbon_all_numeric |>
+  select(-c(date:timestamp))
+
+# calculating correlations 
+carbon_corrs = cor(carbon_all_numeric) |>
+  melt()|>
+  as.data.frame() 
+
+# plotting correlations 
+ggplot(carbon_corrs, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "green", high = "blue", mid = "white", midpoint = 0)
+
+
+
+
+  
