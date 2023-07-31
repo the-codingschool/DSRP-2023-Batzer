d9ee092e34262eaeddcad7623e2b442fd677ec0a
Justin Huang <jhuang454@gmail.com>
2023-07-30 21:35:51 -0400

    datacleaning

--- Justin/scripts/cleaningData.R
+++ Justin/scripts/cleaningData.R
@@ -12,7 +12,9 @@ library(ranger)
 library(Metrics)
 library(reshape2)
 
-nonadata <- filter(data,!is.na)
+#need to make this into dataframe first
+carbondata <- as.data.frame(data)
+head(carbondata)
 
 #CHECK TO MAKE SURE IF 0 MEANS NO DATA
 
@@ -25,8 +27,9 @@ zerodata <- filter(data,value == 0) #view all of the 0 values
 #could try using mutate with an ifelse statement
 
 #Austria
-austriazerodata <- filter(data,country == "Austria", value == 0) #0s in power and industry
-
+austriadata <- filter(carbondata,country == "Austria")
+austriazerodata <- filter(carbondata,country == "Austria", value == 0) #0s in power and industry
+ 
 #find the averages of austria power and industry
 austriadata <- filter(data,country == "Austria")
 austriapower <- filter(austriadata,sector == "Power")
@@ -34,6 +37,24 @@ austriaindustry <- filter(austriadata,sector == "Industry")
 
 austriapowermean <- mean(austriapower$value)
 austriaindustrymean <- mean(austriaindustry$value)
+
+
+austriazerodata <- filter(austriadata,value == 0)
+
+carbondata$value[carbondata$value == 0  & carbondata$country == "Austria" & carbondata$sector== "Power"]<- austriapowermean
+carbondata$value[carbondata$value == 0  &  carbondata$country == "Austria" & carbondata$sector== "Industry"]<-austriaindustrymean
+
+#Latvia
+#power and industry
+
+latviapower <- filter(carbondata,country == "Latvia",sector == "Power")
+latviaindustry <- filter(carbondata,country=="Latvia", sector == "Industry")
+
+latviapowermean <- mean(latviapower$value)
+latviaindustrymean <- mean(latviaindustry$value)
+
+carbondata$value[carbondata$value == 0 & carbondata$country == "Latvia" & carbondata$sector == "Power"] <- latviapowermean
+carbondata$value[carbondata$value == 0 & carbondata$country == "Latvia" & carbondata$sector == "Industry"] <- latviaindustrymean
 
 #Luxembourg
 luxzerodata <- filter(data,country == "Luxembourg", value == 0)
@@ -44,51 +65,86 @@ luxdomesticaviation <- filter(data,country == "Luxembourg",sector == "Domestic A
 luxdomesticaviationmean <- mean(luxdomesticaviation$value)
 
 
-
 #Cyprus
 cyrpuszerodata <- filter(data,country == "Cyprus", value == 0)
 #all zeros in domestic aviation like luxembourg
 #most in 2019 though so not caused by lockdown
 
+cyprusdomesticaviation <- filter(carbondata,country == "Cyprus",sector == "Domestic Aviation")
+cyprusdomesticaviationmean <- mean(cyprusdomesticaviation$value)
 
-
+carbondata$value[carbondata$value== 0 & carbondata$country == "Cyprus" & carbondata$sector == "Domestic Aviation"] <- cyprusdomesticaviationmean
 
 #Lithuania
+lithuaniazerodata <- filter(data,country == "Lithuania",value==0)
+#all zeros in domestic aviation
+lithuaniadomesticaviation <- filter(carbondata,country == "Lithuania",sector == "Domestic Aviation")
+lithuaniaaviationmean <- mean(lithuaniadomesticaviation$value)
 
-
+carbondata$value[carbondata$value == 0 & carbondata$country == "Lithuania" & carbondata$sector == "Domestic Aviation"] <- lithuaniaaviationmean
 
 #Hungary
+hungaryzerodata <- filter(data,country == "Hungary", value == 0)
+#all zeros in domestic aviation for 2019
 
+hungarydomesticaviation <- filter(carbondata,country=="Hungary",sector == "Domestic Aviation")
+hungarydomesticaviationmean <- mean(hungarydomesticaviation$value)
+
+carbondata$value[carbondata$value == 0 & carbondata$country == "Hungary" & carbondata$sector == "Domestic Aviation"] <- hungarydomesticaviationmean
 
 #ask professor batzer about zero values
+# he said that for luxembourg it could be 0 values
+# but otherwise treat 0s as missing values
 
+#can do stuff like anova testing and chi testing to try to find trends
+# among regions or countries
+#and then can do background research to see why these trends are happening
 
-francedata <- filter(data,country == "France")
-germanydata <- filter(data,country == "Germany")
-ukdata <- filter(data,country == "United Kingdom")
-eudata <- filter(data,country != "United Kingdom")
-spaindata <- filter(data,country == "Spain")
-italydata <- filter(data, country == "Italy")
+zeeerodata <- filter(carbondata,value==0)
 
-westerneurope <- filter(data,country %in% c("United Kingdom","France","Germany","Belgium","Belgium",
-                                            "Netherlands","Luxembourg","Austria"))
-northerneurope <- filter(data,country %in% c("Denmark","Sweden","Finland","Estonia","Latvia","Lithuania"))
+populationdata <- worldbank_population_bycountry_data
 
-southerneurope <- filter(data, country %in% c("Spain","Portugal","Italy","Greece","Malta","Cyprus"))
+populationordered <- arrange(populationdata,year,population)
+populationordered
 
-easterneurope <- filter(data,country %in% c("Poland","Czech Republic","Slovakia","Slovenia",
-                                            "Hungary","Croatia","Romania","Bulgaria"))
+population2022 <- filter(populationdata,year == "2022")
+population2022
+# some of the values are wrong so im going to correct them using the world bank website
+population2022$population[population2022$country == "Croatia"] <- 3854000
+population2022$population[population2022$country == "Cyprus"] <- 1251488
+population2022$population[population2022$country == "Czech Republic"] <- 10526073
+population2022$population[population2022$country == "Denmark"] <- 5903037
+population2022$population[population2022$country == "Greece"] <- 10566531
+population2022$population[population2022$country == "Hungary"] <- 9683505
+population2022$population[population2022$country == "Ireland"] <- 5086988
+population2022$population[population2022$country == "Italy"] <- 58856847
+population2022$population[population2022$country == "Latvia"] <- 1883379
+population2022$population[population2022$country == "Lithuania"] <- 2833000
+population2022$population[population2022$country == "Luxembourg"] <- 650774
+population2022$population[population2022$country == "Malta"] <- 523417
+population2022$population[population2022$country == "Netherlands"] <- 17703090
+population2022$population[population2022$country == "Poland"] <- 37561599
+population2022$population[population2022$country == "Portugal"] <- 10379007
+population2022$population[population2022$country == "Romania"] <- 18956666
+population2022$population[population2022$country == "Slovakia"] <- 5431752
+population2022$population[population2022$country == "Slovenia"] <- 2108732
+population2022$population[population2022$country == "Spain"] <- 47615034
+population2022$population[population2022$country == "Sweden"] <- 10486941
+population2022$population[population2022$country == "United Kingdom"] <- 66971411
+population2022$population[population2022$country == "EU27 & UK"] <- 447956050 + 66971411
+populationalphabetical <- arrange(population2022,country)
 
-#seperate into various regions and countries so they can be compared amongst each other and also 
-#so it will be easier to visualize and perform machine learning on them
+# testing
+?pivot_longer
+?pivot_wider
+#Spreading the sectors out 
+sectordata = pivot_wider(carbondata, 
+                          names_from = sector, 
+                          values_from = value)
 
-unique(data$sector)
-francedata
-germanydata
-mean(germanydata$value)
-mean(francedata$value)
+pivottest = pivot_longer(sectordata,cols = c("Power", "Industry", "Ground Transport", "Residential",
+                         "Domestic Aviation", "International Aviation"), names_to = "Sector",values_to = "count")
 
-head(data)
-#need to make this into dataframe first
-carbondata <- as.data.frame(data)
-head(carbondata)
+# idea: make a summary dataset for total values, make a summary dataset for power values
+# divide the average power value column by the total column, mutate it into a new column and then order that
+test <- group_by(sectordata,country)                     
